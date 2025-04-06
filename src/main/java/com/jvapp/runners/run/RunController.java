cff877aba2dbd6b9ea282a837afb69446b2c7df4
LeonNjoroge <leonmuraguri@gmail.com>
2025-04-06 16:32:35 +0300

    Feat: Create Full CRUD functionality

--- src/main/java/com/jvapp/runners/run/RunController.java
+++ src/main/java/com/jvapp/runners/run/RunController.java
@@ -1,10 +1,12 @@
 package com.jvapp.runners.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -13,15 +15,42 @@ public class RunController {
     private final RunRepository runRepository;
 
     public RunController(RunRepository runRepository){
+
         this.runRepository = runRepository;
     }
+    // Get
     @GetMapping("")
     List<Run> findAll(){
         return runRepository.findAll();
     }
     @GetMapping("/{id}")
-    Run findById(@PathVariable Integer id){
-        return runRepository.findById(id);
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException((HttpStatus.NOT_FOUND));
+        }
+        return run.get();
+    }
+
+    // Post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run){
+        runRepository.create(run);
+    }
+
+    // Put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id){
+        runRepository.update(run,id);
+    }
+
+    // Delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
     }
 
 }
--- src/main/java/com/jvapp/runners/run/RunRepository.java
+++ src/main/java/com/jvapp/runners/run/RunRepository.java
@@ -4,10 +4,13 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
@@ -19,12 +22,27 @@ public class RunRepository {
         return runs;
     }
 
-    Run findById(Integer id){
+    Optional<Run> findById(Integer id){
         return runs.stream()
                 .filter(run -> run.id() == id)
-                .findFirst()
-                .get();
+                .findFirst();
     }
+
+    void create(Run run){
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if(existingRun.isPresent()){
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id){
+        runs.removeIf(run -> run.id().equals(id));
+    }
+
 
     @PostConstruct
     private void init(){
